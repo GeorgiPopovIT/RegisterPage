d7d459a5a235ecddc212fa6a80c45cd12f876c92
Georgi Popov <gogopopo262003@gmail.com>
2020-09-07 14:45:21 +0300

    Second change

---
+++ SQLQuery1.sql
@@ -0,0 +1,28 @@
+CREATE PROC UserAddOrEdit
+@UserID int,
+@FirstName varchar(50),
+@LastName varchar(50),
+@Contact varchar(50),
+@Gender varchar(10),
+@Address varchar(250),
+@Username varchar(50),
+@Password varchar(50)
+AS
+	IF @UserID = 0
+	BEGIN
+		INSERT INTO UserRegistration(FirstName,LastName,Contact,Gender,Address,Username,Password)
+		VALUES (@FirstName,@LastName,@Contact,@Gender,@Address,@Username,@Password)
+	END
+	ELSE
+	BEGIN
+		UPDATE UserRegistration
+		SET
+			FirstName = @FirstName,
+			LastName = @LastName,
+			Contact = @Contact,
+			Gender = @Gender,
+			Address = @Address,
+			Username = @Username,
+			Password = @Password
+			WHERE UserID = @UserID
+	END
